8d2e65d1a94b441d85c6acab3d62f08af0db0e87
David Caicedo <davidfcc7@gmail.com>
2021-06-17 17:57:26 -0500

    operador logico

--- operaciones.py
+++ operaciones.py
@@ -12,3 +12,16 @@ numero2 = '4'
 numero2 = int(numero2)
 resultado = numero1 + numero2
 print(resultado)
+
+# operadores logicos and, or, not
+estudiante = True
+trabaja = False
+trabajar = not trabaja
+persona = estudiante and trabajar
+print(persona)
+
+numeroUno = 5
+numeroDos = 5
+
+numeros = numeroUno == numeroDos
+print(numeros)
