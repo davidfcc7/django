b6d7af38e4764cee3706c90118b0891a7ab1780f
David Caicedo <davidfcc7@gmail.com>
2021-06-17 17:29:45 -0500

    de string a numero

--- operaciones.py
+++ operaciones.py
@@ -1,3 +1,14 @@
+#libreria
 import math;
-
 print (math.sqrt(16))
+
+# variable asignacion
+nombre = 'david'
+print(nombre)
+
+# convertir un string a numero entero y poder sumarla con otro entero
+numero1 = 4
+numero2 = '4'
+numero2 = int(numero2)
+resultado = numero1 + numero2
+print(resultado)
