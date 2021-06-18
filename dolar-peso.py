e16c2198558161b81d7c66ee5b809de5e0ea6783
David Caicedo <davidfcc7@gmail.com>
2021-06-17 19:47:14 -0500

    de pesos a dolares

--- dolar-peso.py
+++ dolar-peso.py
@@ -1,7 +1,9 @@
 pesos = input('pesos colombianos: ')
-pesos = int(pesos)
+pesos = float(pesos)
 dolar = 3600
 
 conversion = pesos / dolar
+conversion = round(conversion, 2) #limitar la cantiad de decimales
+conversion = str(conversion)
 
-print(conversion)
+print('tienes $' + conversion + ' dolares')
